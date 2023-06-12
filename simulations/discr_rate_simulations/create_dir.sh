25c20d9104806f7ade6d6e6690bf86ba7b23c490
Gianmarco Tiddia <tiddia2@jsfl01.jusuf>
2023-06-12 10:18:38 +0200

    added_simulation_data

---
+++ simulations/discr_rate_simulations/create_dir.sh
@@ -0,0 +1,13 @@
+for i in {1..20}; do
+    j=$(($i*5000))
+    cp -r template $j
+    cd $j
+    sed "s/__PATTERNS__/$j/" params.templ > params.dat
+    
+    net=$(printf "network_000*_%04d.dat" $(($i-1)))
+    ln -s /p/scratch/icei-hbp-2023-0002/pi_step100_discr_C5000_T100000/$net .
+
+    . make_structural_plasticity.sh
+    
+    cd ..    
+done
---
+++ simulations/noise_1Hz_simulations/90000/make_structural_plasticity.sh
@@ -0,0 +1,1 @@
+g++ -Wall  -O4 -fopenmp -o test_structural_plasticity test_structural_plasticity.cpp structural_plasticity.cpp erfinv.cpp
