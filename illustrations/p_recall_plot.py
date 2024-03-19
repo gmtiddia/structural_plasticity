a273d84305efb64f5d332c7b59f4d3c792781a4a
gmtiddia <gianmarcotiddia@gmail.com>
2024-03-19 10:21:01 +0100

    edited illusatration of P_C

--- illustrations/p_recall_plot.py
+++ illustrations/p_recall_plot.py
@@ -17,15 +17,15 @@ sdnr_thr = erfinv(2*p_thr-1)*np.sqrt(8)
 y_values = 0.5 + 0.5*erf(x_values/np.sqrt(8))
 
 #y1=erfinv(x_values/np.sqrt(8))-erfinv(x_values/(3*np.sqrt(8)))
-legend_fs =15
-tick_fs = 15
-tick_text= 18
+legend_fs =13
+tick_fs = 13
+tick_text= 17
 
 #plt.rcParams['axes.linewidth'] = 1.5 # Spessore del box
 plt.plot(x_values, y_values, label=R'$P_C$',color='black',linewidth=1.5)
 plt.hlines(y=0.95, xmin=0, xmax=sdnr_thr, color='red', linestyle='--',label=r'$P_C=95\%$')
 plt.vlines(x=sdnr_thr, ymin=0.5, ymax=0.95, color='blue', linestyle=':')
-plt.text(sdnr_thr, 0.55, r'$SDNR_{thr}$', color='blue', ha='center', va='center', bbox=dict(facecolor='white', edgecolor='white', boxstyle='round,pad=0.5'),fontsize=tick_fs)
+plt.text(sdnr_thr, 0.55, r'$\text{SDNR}_{thr}$', color='blue', ha='center', va='center', bbox=dict(facecolor='white', edgecolor='white', boxstyle='round,pad=0.5'),fontsize=tick_fs)
 plt.text(1, 0.95, r'$P_{C}$', color='red',  ha='center', va='center', bbox=dict(facecolor='white', edgecolor='white', boxstyle='round,pad=0.5'),fontsize=tick_fs)
 plt.title('')
 plt.xlim(0,7)
